8dd34bb8e937f685c75187c16beacf641d47c680
Sachin <sachink1630@gmail.com>
2024-12-13 09:31:38 +1100

    Week 1 task

---
+++ WEEK1/28thFebExample.py
@@ -0,0 +1,17 @@
+"""
+Program to print Hello World / any other string 
+"""
+
+__author__ = "Sachin Kharel"
+
+#def greet_users(greet: str):
+#    "prints the string received by greet in a signle line"
+#    print(greet)
+
+def main():
+    print("Hello World!")
+    print("Have a good day!")
+    
+
+if __name__ == "__main__":
+    main()
