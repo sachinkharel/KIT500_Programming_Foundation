1cbccd48ebe2daf7d85379d7cf00c2e4a169f097
Sachin <sachink1630@gmail.com>
2024-12-16 18:04:08 +1100

    updated using idioms

--- Practice/commonnames.py
+++ Practice/commonnames.py
@@ -8,10 +8,11 @@ def main():
     namedict = dict()
     listdict = ['sachin', 'suman', 'sachin', 'subash', 'sachin', 'suman', 'suman', 'sachin']
     for name in listdict:
-        if name not in namedict:
-            namedict[name] = 1
-        else:
-            namedict[name] = namedict[name] + 1
+        # if name not in namedict:
+        #     namedict[name] = 1
+        # else:
+        #     namedict[name] = namedict[name] + 1
+        namedict[name] = namedict.get(name,0) + 1   #using idiom to count the names
     print(namedict)
 
 
