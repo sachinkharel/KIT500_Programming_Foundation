f4acc5e5a4ed08d35944f2055b4395d97b44cdeb
Sachin <sachink1630@gmail.com>
2025-03-27 21:53:40 +1100

    completed week 4 and started week 5

--- Practice/test.py
+++ Practice/test.py
@@ -9,7 +9,21 @@ __author__ = "Sachin Kharel"
 #    print(greet)
 
 def main():
-  
+    
+    print(search_target([ 12, 45, 3, 18, 25], 18))
+
+def search_target(data: list[int], target: int) -> bool: # Function to search for a target in a list.
+    index = 0
+    stopped = False
+
+    while index < len(data) and not stopped:
+        if data[index] == target:
+            stopped = True
+        index += 1
+
+    return stopped
+
+   
     
 
 if __name__ == "__main__":
