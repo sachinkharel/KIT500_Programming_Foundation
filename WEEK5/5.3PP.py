86b22ddf5878238a5c53b9a489d17b10a0ee7cd6
Sachin <sachink1630@gmail.com>
2025-03-31 21:55:58 +1100

    added 5.3 Spot the mistake answer sheet

---
+++ WEEK5/5.3PP.py
@@ -0,0 +1,20 @@
+"""
+5.3PP Test template
+"""
+
+__author__ = "Sachin Kharel"
+
+
+def main():
+    a: int # a number
+    b: int # another number
+    c: int # yet another number
+
+    a = 18
+
+    c = 27
+    b = a * c
+    print(f"a is {a}, b is {b}, and c is {c}")
+        
+if __name__ == "__main__":
+    main()
