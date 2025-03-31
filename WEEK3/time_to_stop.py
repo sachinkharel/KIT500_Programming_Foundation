991464b6ea720a6dec1ca998b894606bbb3a3e3c
Sachin <sachink1630@gmail.com>
2025-03-31 16:50:21 +1100

    updated 3.3PP to include comments

--- WEEK3/time_to_stop.py
+++ WEEK3/time_to_stop.py
@@ -14,15 +14,15 @@ def calculate_time_to_stop(mass:float, velocity: float, braking_force: float) ->
 
 def main():
     
-    mass1:float = 1500
-    velocity1:float = 25
-    braking_force1:float = 6000
+    mass1:float = 1500 #mass of vehicle 1
+    velocity1:float = 25 #initial velocity of vehicle 1
+    braking_force1:float = 6000 #braking force of vehicle 1
     
-    mass2:float = 1000
-    velocity2:float = 15
-    braking_force2:float = 4000
+    mass2:float = 1000 #mass of vehicle 2
+    velocity2:float = 15 #initial velocity of vehicle 2
+    braking_force2:float = 4000 #braking force of vehicle 2
     
-    
+    #displaying the time required to stop for both vehicles
     print("Time Required to Stop")
     
     print()
@@ -32,13 +32,14 @@ def main():
     
     print()
     
-    mass = float(input("Input vehicle mass (kg): "))
-    velocity = float(input("Input initial velocity (m/s): "))
-    braking_force = float(input("Input braking force (N): "))
+    mass = float(input("Input vehicle mass (kg): ")) #taking input for mass
+    velocity = float(input("Input initial velocity (m/s): ")) #taking input for initial velocity
+    braking_force = float(input("Input braking force (N): ")) #taking input for braking force
     
     print()
     print("Calculating...")
     
+    #displaying the time required to stop for the vehicle using the input values
     print(f"Time to Stop: {calculate_time_to_stop(mass,velocity,braking_force):.2f} seconds")
     
     
