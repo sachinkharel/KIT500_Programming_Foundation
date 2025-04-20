36ff5b105257f551ff97df9ba37cff68084fb314
Sachin <sachink1630@gmail.com>
2025-04-20 20:13:34 +1000

    moved main to a different file

---
+++ WEEK7/7.6_resume_updater/main.py
@@ -0,0 +1,30 @@
+from parser.parser import ResumeParser
+from models.enums import FileType
+
+
+def main():
+    """
+    Main function to initialize the parser and start the parsing process.
+    It asks for the path to the resume file and checks if the file is in the correct format.
+    """
+    
+    # Prompt the user for the path to the resume file
+    
+    resume_file = input("Enter the path to your resume file (e.g., resume.docx): ")
+    
+    # Check until user provides a valid file path
+    
+    while not resume_file.endswith(FileType.DOCX.value):
+        print("Invalid file format. Please provide a .docx file.")
+        resume_file = input("Enter the path to your resume file (e.g., resume.docx): ")
+
+    
+    # Initialize the ResumeParser with the provided resume file
+    parser = ResumeParser(resume_file)
+    
+    # Parse the content of the resume
+    parser.parse()
+    
+if __name__ == "__main__":
+    # Run the main function when the script is executed
+    main()
